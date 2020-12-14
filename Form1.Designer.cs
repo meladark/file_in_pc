dc4c2aa3a90cd7e597f4237f9129b5d8eb1d121c
Norfolk <mikedark@yandex.ru>
2020-12-14 13:41:50 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace file_in_pc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(571, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Поиск файла";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(553, 434);
+            this.treeView1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(652, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Закончить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(571, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Время выполнения";
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Всего найдено файлов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(571, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Всего файлов";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(571, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(571, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Текущая директория";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(574, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Директория";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(574, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(571, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Выбранная директория";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(571, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Продолжить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 461);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+namespace file_in_pc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        string path = "";
+        string rex = "";
+        string setting_file = "set.dat";
+        List<string> not_over = new List<string>();
+        int counter = 0, fi = 0;
+        List<string> result = new List<string>();
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            find_file find_File = new find_file();           
+            find_File.find(path, rex, ref counter, ref fi, ref result, this, ref not_over);
+            backgroundWorker2.WorkerSupportsCancellation = true;
+            backgroundWorker2.CancelAsync();
+            button1.BeginInvoke((Action)(() => button1.Enabled = true));
+            button2.BeginInvoke((Action)(() => button2.Enabled = false));
+        }
+
+        /// <summary>
+        /// Функция прорисовки на treeview
+        /// </summary>
+        /// <param name="files"> Найденные файлы в выбранной папке </param>
+        /// <returns> Возвращает флаг прерывания потока: true - поток вызван к завершению; false - иначе. </returns> 
+        public bool View_it_tree(string[] files)
+        {
+            if (files.Length == 0)
+            {
+                if (backgroundWorker1.CancellationPending || backgroundWorker3.CancellationPending) { return true; } else { return false; }
+            }
+            treeView1.Invoke((Action)(() => treeView1.BeginUpdate()));
+            long ntime = DateTime.Now.Ticks;
+            foreach (var fi in files)
+            {
+                if (backgroundWorker1.CancellationPending || backgroundWorker3.CancellationPending)
+                {
+                    treeView1.Invoke((Action)(() => treeView1.EndUpdate()));
+                    return true; // вызов прекращения потока, вызывается при прорисовки каждого файла
+                } 
+                List<string> s = fi.Split('\\').ToList();
+                TreeNode[] all = null;
+                TreeNode nowN = null;
+                try
+                {
+                    if (treeView1.Nodes[0].Text.Equals(s[0])) { nowN = treeView1.Nodes[0]; } else { treeView1.Invoke((Action)(() => nowN = treeView1.Nodes.Add(s[0], s[0]))); }
+                }
+                catch
+                {
+                    treeView1.Invoke((Action)(() => nowN = treeView1.Nodes.Add(s[0], s[0])));
+                    nowN = treeView1.Nodes[0];
+                }
+                for (int j = 1; j < s.Count; j++)
+                {   
+                    string k = s[j];
+                    treeView1.Invoke((Action)(() => all = nowN.Nodes.Find(k, false)));
+                    if (all != null)
+                        if (all.Length > 0)
+                        {
+                            nowN = all[0];
+                        } else
+                        {
+                            treeView1.Invoke((Action)(() => nowN = nowN.Nodes.Add(k, k))); 
+                            
+                        }  
+                }
+                if ((DateTime.Now.Ticks - ntime)/TimeSpan.TicksPerMillisecond >= 17) //происовка чтобы не мерцало
+                {
+                    ntime = DateTime.Now.Ticks;
+                    treeView1.Invoke((Action)(() => treeView1.EndUpdate()));
+                }            
+            }            
+            treeView1.Invoke((Action)(() => treeView1.EndUpdate()));
+            label3.Invoke((Action)(() => label3.Text = (Convert.ToInt32(label3.Text) + files.Length).ToString()));
+            return false;
+        }
+
+        public bool view_all_file(int count)
+        {
+            if (backgroundWorker1.CancellationPending || backgroundWorker3.CancellationPending) { return true; } 
+                if (count == 0)
+                {
+                    return false;
+                } else
+            {
+                label4.Invoke((Action)(() => label4.Text = (Convert.ToInt32(label4.Text) + count).ToString()));
+            }
+            return false;
+        }
+
+        public void View_dir(string dir)
+        {
+            label6.Invoke((Action)(() => label6.Text = dir));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            not_over = new List<string>();
+            counter = 0;
+            fi = 0;
+            result = new List<string>();
+            if (textBox1.Text != rex)
+            {
+                rex = textBox1.Text;
+                string[] f = File.ReadAllLines(setting_file);
+                for (int i = 0; i < f.Length; i++)
+                {
+                    if (f[i].Contains("rex = ")) { f[i] = "rex = " + rex; }
+                }
+                File.WriteAllLines(setting_file, f);
+            }
+            treeView1.Nodes.Clear();
+            label3.Text = "0";
+            label4.Text = "0";
+            label6.Text = "//";
+            label2.Text = DateTime.MinValue.ToString("HH:mm:ss:ffff");
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button4.Enabled = false;
+            
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.RunWorkerAsync(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            try
+            {
+                string[] f = File.ReadAllLines(setting_file);
+                foreach (var line in f)
+                {
+                    if (line.Contains("path = "))
+                    {
+                        path = line.Substring(7);
+                    }
+                    if (line.Contains("rex = "))
+                    {
+                        rex = line.Substring(6);                    
+                    }
+                }
+            }
+            catch
+            {
+                FileStream file = File.Create(setting_file);
+                byte[] ini = new UTF8Encoding(true).GetBytes("path = \"\"\nrex = \"\"");
+                file.Write(ini, 0, ini.Length);
+                path = "";
+                rex = "";
+                file.Close();
+            }
+            label7.Text = path;
+            textBox1.Text = rex;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.WorkerSupportsCancellation = true;
+                backgroundWorker1.CancelAsync();
+            }
+            if (backgroundWorker2.IsBusy)
+            {
+                backgroundWorker2.WorkerSupportsCancellation = true;
+                backgroundWorker2.CancelAsync();
+            }
+            if (backgroundWorker3.IsBusy)
+            {
+                backgroundWorker3.WorkerSupportsCancellation = true;
+                backgroundWorker3.CancelAsync();
+            }
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = true;
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            TimeSpan data;
+            try
+            {
+                data = DateTime.Now.Subtract(DateTime.ParseExact(label2.Text, "HH:mm:ss:ffff", provider));
+            }
+            catch
+            {
+                data = new TimeSpan(DateTime.Now.Ticks);
+            }         
+            label2.Invoke((Action)(() => label2.Text = data.ToString()));
+            do
+            {
+                if (backgroundWorker2.CancellationPending)
+                    return;
+                label2.BeginInvoke((Action)(() => label2.Text = DateTime.Now.Subtract(data).ToString("HH:mm:ss:ffff")));
+                Thread.Sleep(10);
+            } while (true);
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < not_over.Count; i++)
+            {
+                if (backgroundWorker3.CancellationPending)
+                    return;
+                string sub = not_over[0];
+                not_over.RemoveAt(0);
+                find_file find_File = new find_file();               
+                find_File.find(sub, rex, ref counter, ref fi, ref result, this, ref not_over);
+            }
+            backgroundWorker2.WorkerSupportsCancellation = true;
+            backgroundWorker2.CancelAsync();
+            button1.BeginInvoke((Action)(() => button1.Enabled = true));
+            button2.BeginInvoke((Action)(() => button2.Enabled = false));
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+                label7.Text = path;
+                string[] f = File.ReadAllLines(setting_file);
+                for (int i = 0; i < f.Length; i++)
+                {
+                    if (f[i].Contains("path = ")) { f[i] = "path = " + path; }
+                }
+                File.WriteAllLines(setting_file, f);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            button2.Enabled = true;
+            button1.Enabled = false;
+            backgroundWorker3.RunWorkerAsync();
+            backgroundWorker2.RunWorkerAsync();
+        }
+    }
+}
